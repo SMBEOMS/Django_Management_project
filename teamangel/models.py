5f30f93a79604cfb197ce76b6e2896d30c31d759
BEOMS <jbs980612@gmail.com>
2023-09-17 16:35:48 +0900

    경영프로젝트 두번째

--- teamangel/models.py
+++ teamangel/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db import models
 
 # Create your models here.
@@ -8,6 +10,8 @@ class Post(models.Model):
     hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='teamangel/images/%Y/%m/%d', blank=True)  # 이미지 폴더 지정하기
+    file_upload = models.FileField(upload_to='teamangel/files/%Y/%m/%d', blank=True)  # 포스트에 파일 올리기
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -16,3 +20,9 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/teamangel/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]  # -1의미는 확장자 의미를함 EX) ~~.html txt py doc csv
--- teamangel/views.py
+++ teamangel/views.py
@@ -1,22 +1,8 @@
-from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    return render(
-        request,
-        'teamangel/index.html',
-        {
-            'posts' : posts,
-        }
-    )
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return  render(
-        request,
-        'teamangel/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+class PostDetail(DetailView):
+    model = Post
