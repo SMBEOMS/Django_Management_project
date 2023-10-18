bf7ed1bc04e81b2afecd666188ec983e4821876f
BEOMS <jbs980612@gmail.com>
2023-10-19 03:02:54 +0900

    서브 카테고리

--- teamangel/models.py
+++ teamangel/models.py
@@ -5,9 +5,12 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
